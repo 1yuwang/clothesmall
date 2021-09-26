966829281f671265e3bc17a4a55b8e7dd3c58902
yuwang <614649899@qq.com>
2021-09-26 09:20:17 +0800

    创建项目

---
+++ src/views/category/Category.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>haha</h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Category",
+  }  
+    
+
+     
+   
+</script>
+
+<style scoped>
+
+</style>
